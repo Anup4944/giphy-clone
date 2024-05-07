a5ca7b8fb6298590925629de42e62879d712088f
Anup <anuppoudel60@gmail.com>
2024-05-07 15:25:52 +1000

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
+import Categories from "./pages/Categories";
+import Single from "./pages/Single";
+import Search from "./pages/Search";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/favourites",
+        element: <Favourites />,
+      },
+      {
+        path: "/:category",
+        element: <Categories />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <Single />,
+      },
+      {
+        path: "/search/:query",
+        element: <Search />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
